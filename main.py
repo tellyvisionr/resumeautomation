2ad5d8a7054040467a87bb0007cd06addd59fec7
Telly Reames <tellyreames2@gmail.com>
2026-02-07 21:56:52 -0800

    Script complete

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import time
 import shutil
 
-downloads_path = "c:\Users\telly\Downloads"
-resume_folder = "C:\Users\telly\OneDrive\Desktop\resume"
+downloads_path = r"c:\Users\telly\Downloads"
+resume_folder = r"C:\Users\telly\OneDrive\Desktop\resume"
 
 old_files = set(os.listdir(downloads_path)) # get initial files list
 
@@ -13,5 +13,14 @@ while True:
     new_files = set(os.listdir(downloads_path))
 
     added_files = new_files - old_files
+
+    for file in added_files:
+
+
+        if "resume" in file.casefold():
+            source = os.path.join(downloads_path, file)
+            destination = os.path.join(resume_folder, file)
+            shutil.move(source, destination)           
+    old_files = new_files
 
     
